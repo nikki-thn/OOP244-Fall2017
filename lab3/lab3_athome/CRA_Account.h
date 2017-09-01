e1cce0318e9e6c7f1532b9e5ddbb92b8c7e73b4b
nikki-thn <ntruong4@myseneca.ca>
2017-09-01 14:22:42 -0400

    Update CRA_Account.h

--- lab3/lab3_athome/CRA_Account.h
+++ lab3/lab3_athome/CRA_Account.h
@@ -1,1 +1,36 @@
+#ifndef SICT_CRA_ACCOUNT_H
+#define SICT_CRA_ACCOUNT_H
 
+#define MAX_NAME_LENGTH 40
+#define MIN_SIN 100000000
+#define MAX_SIN 999999999
+#define MAX_YRS 4
+
+namespace sict {
+
+	class CRA_Account {
+
+		char lastName[MAX_NAME_LENGTH + 1];
+		char firstName[MAX_NAME_LENGTH + 1];
+		int sinNum;
+		int m_year[MAX_YRS];
+		double m_balance[MAX_YRS];
+		int m_years;
+
+
+	public:
+
+		void set(const char* familyName, const char* givenName, int sin);
+		bool isValid() const;
+		void display() const;
+
+		void set(int year, double balance);
+
+	};
+
+
+
+}
+
+
+#endif
