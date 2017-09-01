0005b216d19529a3cc086ff49aa16a2c4acdc16a
nikki-thn <ntruong4@myseneca.ca>
2017-09-01 09:27:57 -0400

    Create CRA_Account.h

---
+++ lab3/CRA_Account.h
@@ -0,0 +1,17 @@
+#ifndef SICT_CRA_ACCOUNT_H
+#define SICT_CRA_ACCOUNT_H
+
+#define MAX_NAME_LENGTH 40
+#define MIN_SIN 100000000
+#define MAX_SIN 999999999
+
+namespace sict {
+
+
+
+
+
+}
+
+
+#endif
