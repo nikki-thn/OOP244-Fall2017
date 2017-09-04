e49e1d435049f55e05c12877a572b353361b0004
nikki-thn <ntruong4@myseneca.ca>
2017-09-04 13:47:08 -0400

    Create Kingdom.h

---
+++ lab2/lab2_inlab/Kingdom.h
@@ -0,0 +1,20 @@
+#ifndef SICT_KINGDOM_H
+#define SICT_KINGDOM_H
+
+namespace sict {
+
+	struct Kingdom {
+
+		char m_name[32];
+		int m_population;
+};
+
+	void display (const Kingdom);
+
+
+
+}
+
+
+#endif // !SICT_KINGDOM_H
+
