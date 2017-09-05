ec87db5e68bba5e68c861cea30aa355750b3e939
nikki-thn <ntruong4@myseneca.ca>
2017-09-05 13:39:32 -0400

    Create w4_at_home.cpp

---
+++ lab4/lab4_athome/w4_at_home.cpp
@@ -0,0 +1,122 @@
+// TODO: add your headers here 
+#include<iostream>
+#include"Passenger.h"
+
+// TODO: continue your namespace here
+using namespace std;
+
+namespace sict {
+
+	// TODO: implement the default constructor here
+	Passenger::Passenger() {
+
+		passengerName[0] = '\0';
+		destination[0] = '\0';
+		
+		departDay = 0;
+		departMonth = 0;
+		departYear = 0;
+
+	}
+
+	//Constructor with 5 parameters here
+
+	Passenger::Passenger(const char *name_, const char* destination_, int year, int month, int day) {
+
+		if (name_ != nullptr && destination_ != nullptr &&
+			name_ != "" && destination_ != "" &&
+			day <= 31 && day >= 1 && 
+			month <= 12 && month >= 1 &&
+			year <= 2020 && year >= 2017){
+
+			strcpy(passengerName, name_);
+			strcpy(destination, destination_);
+			departMonth = month;
+			departYear = year;
+			departDay = day;
+
+	
+		}
+
+		else {
+
+			passengerName[0] = '\0';
+
+		}
+
+
+
+	}
+
+	// TODO: implement the constructor with 2 parameters here
+	Passenger::Passenger(const char* name_, const char* destination_) {
+	
+		if (name_ != nullptr && destination_ != nullptr) {
+
+			departDay = 1;
+			departMonth = 7;
+			departYear = 2017;
+
+		}
+
+		else {
+
+			passengerName[0] = '\0';
+
+		}
+
+	}
+
+
+	// TODO: implement isEmpty query here
+	bool Passenger::isEmpty() const {
+
+		bool isEmpty = true;
+
+		if (passengerName[0] != '\0') {
+
+			isEmpty = false;
+
+		}
+
+		//cout << "empty" << isEmpty << endl;
+
+		return isEmpty;
+	}
+
+
+	// TODO: implement display query here
+	void Passenger::display() const {
+
+		cout << passengerName << " - " << destination;
+		cout << " on" << departYear << "/";
+		cout.fill('0');
+		cout.width(2);
+		cout << departMonth << "/" << departDay << endl;
+
+	}
+/*
+	const char* Passenger::name() const {
+
+		bool validCheck = isEmpty();
+		char* addressOfName = nullptr;
+
+		if (validCheck == true) {
+
+		
+		}
+
+		return addressOfName;
+
+	}
+
+
+	bool Passenger::canTravelWith(const Passenger & name) const {
+		
+		bool canTravel = true;
+		char* currentPassenger = nullptr;
+
+		if (name.destination == david.destincation)
+	}
+	*/
+}
