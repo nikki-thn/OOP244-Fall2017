dcaeaee6a3796092c8af3e2dcb62ed3aba916575
nikki-thn <ntruong4@myseneca.ca>
2017-09-05 10:39:23 -0400

    Update Passenger.h

--- lab4/lab4_inlab/Passenger.h
+++ lab4/lab4_inlab/Passenger.h
@@ -1,1 +1,29 @@
-d
+// TODO: add header file guard here 
+#ifndef SICT_PASSENGER_H
+#define SICT_PASSENGER_H
+
+#define SIZE 32
+
+
+// TODO: declare your namespace here
+namespace sict {
+
+	// TODO: define the Passenger class here
+	class Passenger {
+
+		char name[SIZE + 1];
+		char destination[SIZE + 1];
+
+	public:
+
+		Passenger(); //default constructor
+		Passenger(const char *name_, const char* destination_); //overload constructor
+
+		bool isEmpty() const;
+		void display() const;
+
+	};
+
+}
+
+#endif
