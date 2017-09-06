bdda0bf7bea803b1126b61690839a5c1197fe46e
nikki-thn <ntruong4@myseneca.ca>
2017-09-06 13:59:24 -0400

    Update Fraction.cpp

--- lab5/lab5_inlab/Fraction.cpp
+++ lab5/lab5_inlab/Fraction.cpp
@@ -1,32 +1,78 @@
+/*
+Nikki Truong - 112 214 174
+OOP244 - Fall 2017
+Workshop 5 - In lab
+*/
+
 // TODO: insert header files
 #include<iostream>
 #include"Fraction.h"
 
+using namespace std;
 // TODO: continue the namespace
 namespace sict {
 
 	// TODO: implement the default constructor
 	Fraction::Fraction() {
-
+		numer = 0;
+		deno = 0;
 	}
 
 	// TODO: implement the two-argument constructor
 	Fraction::Fraction(int nn, int nd) {
+		
+		//data valid if numerator is non-negative and denomeator is positive
+		bool valid = nn >= 0 && nd > 0;
 
+		if (valid == true) {
+			
+			//copy data to object when valid
+			numer = nn;
+			deno = nd;
+		}
+
+		if (valid == false) {
+
+			Fraction(); //set object to safety state if data are invalid
+		}
 	}
 
 	// TODO: implement the max query
 	// max returns the maximum of the numerator and denominator
 	//
 	int Fraction::max() const {
+		int greater = 0;
 
+		if (numer >= deno) {
+
+			greater = numer;
+		}
+
+		if (numer < deno) {
+
+			greater = deno;
+		}
+
+		return greater;
 	}
 
 	// TODO: implement the min query
 	// min returns the minimum of the numerator and denominator
 	//
 	int Fraction::min() const {
+		int lesser = 0;
 
+		if (numer >= deno) {
+
+			lesser = deno;
+		}
+
+		if (numer < deno) {
+
+			lesser = numer;
+		}
+
+		return lesser;
 	}
 
 	// gcd returns the greatest common divisor of num and denom
@@ -51,6 +97,7 @@ namespace sict {
 	//
 	void Fraction::reduce() {
 
+
 	}
 
 	// TODO: implement the display query
@@ -58,6 +105,19 @@ namespace sict {
 	//
 	void Fraction::display() const {
 
+		bool empty = isEmpty();
+
+		cout << numer << "/" << deno << endl;
+
+		if (deno == 1) {
+
+			cout << numer << endl;
+		}
+
+		if (empty == true) {
+
+			cout << "no fraction stored" << endl;
+		}
 
 	}
 
@@ -67,14 +127,26 @@ namespace sict {
 	//
 	bool Fraction::isEmpty() const {
 
+		bool emptyCheck = true;
+
+		if (numer != 0 && deno != 0) {
+
+			emptyCheck = false;
+		}
+
+		return emptyCheck;
+
 	}
 
 	// TODO: implement the + operator
 	// + operator adds the rhs to the current object and reduces the result
 	//
-	Fraction& Fraction::operator+() {
+	Fraction Fraction::operator+ (int& num) {
+
+		int result = 0;
 
 
+		return Fraction(numer, deno);
 	}
 
 
