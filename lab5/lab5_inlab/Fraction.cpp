4ceb7c218a06a6a1a98ca2bb691f31131e2b2a38
nikki-thn <ntruong4@myseneca.ca>
2017-09-06 12:58:51 -0400

    Update Fraction.cpp

--- lab5/lab5_inlab/Fraction.cpp
+++ lab5/lab5_inlab/Fraction.cpp
@@ -6,16 +6,28 @@
 namespace sict {
 
 	// TODO: implement the default constructor
+	Fraction::Fraction() {
+
+	}
 
 	// TODO: implement the two-argument constructor
+	Fraction::Fraction(int nn, int nd) {
+
+	}
 
 	// TODO: implement the max query
 	// max returns the maximum of the numerator and denominator
 	//
+	int Fraction::max() const {
+
+	}
 
 	// TODO: implement the min query
 	// min returns the minimum of the numerator and denominator
 	//
+	int Fraction::min() const {
+
+	}
 
 	// gcd returns the greatest common divisor of num and denom
 	//
@@ -44,15 +56,26 @@ namespace sict {
 	// TODO: implement the display query
 	// display inserts num/denom into the output stream
 	//
+	void Fraction::display() const {
+
+
+	}
 
 
 	// TODO: implement the isEmpty query
 	// isEmpty returns true if Fraction object is in a safe empty state
 	//
+	bool Fraction::isEmpty() const {
+
+	}
 
 	// TODO: implement the + operator
 	// + operator adds the rhs to the current object and reduces the result
 	//
+	Fraction& Fraction::operator+() {
+
+
+	}
 
 
 }
