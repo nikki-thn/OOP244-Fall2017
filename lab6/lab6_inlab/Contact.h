b4f8bc7e05a337028a309ced7fb206ac983b1e9f
nikki-thn <ntruong4@myseneca.ca>
2017-09-13 19:52:06 -0400

    Create Contact.h

---
+++ lab6/lab6_inlab/Contact.h
@@ -0,0 +1,36 @@
+#ifndef SICT_CONTACT_H
+#define SICT_CONTACT_H
+
+
+namespace sict {
+
+	class Contact {
+
+		char name[21];
+		int numOfPhones;
+	
+		long long* phoneNum = new long long [numOfPhones];
+
+	public:
+
+		Contact();
+		Contact(const char*, const long long*, int);
+		~Contact();
+
+		bool isEmpty() const;
+		void display() const;
+
+	};
+
+	//exact number
+	//bool exact(long long);
+
+
+}
+
+
+#endif
+
+
+//Dynamic allocated array http://www.cs.fsu.edu/~jestes/cop3330/notes/dma.html 
+//Another link https://stackoverflow.com/questions/15369913/dynamic-arrays-inside-a-class
