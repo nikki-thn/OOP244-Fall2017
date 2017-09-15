963c832a0dd07fb1584a51443732a8687f3af02b
nikki-thn <ntruong4@myseneca.ca>
2017-09-15 13:47:01 -0400

    Create example.c

---
+++ example.c
@@ -0,0 +1,9 @@
+//create references
+int main() {
+     double a = 10.0;
+     double& rA = a;
+     
+     cout << 'A: " << a << endl;
+     cout << 'rA: " << rA << endl;
+     //same memory location, produce same outputs
+}
