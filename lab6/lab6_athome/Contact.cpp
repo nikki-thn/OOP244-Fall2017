a4b379fa753a8610a7f44ec2b5bd48e9e45b2eba
nikki-thn <ntruong4@myseneca.ca>
2017-09-16 18:53:02 -0400

    Update Contact.cpp

--- lab6/lab6_athome/Contact.cpp
+++ lab6/lab6_athome/Contact.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include "Contact.h"
 
 using namespace std;
@@ -14,23 +15,27 @@ namespace sict {
 
 	Contact::Contact(const char* sourceName, const long long* sourcePhone, int size) {
 
+		*this = Contact();
 		phoneNum = new long long[size];
+		bool valid;
 
-		if (sourceName != nullptr && size != 0) {
+		if (sourceName != nullptr) {
 
-			//****problem, can't copy oversize character
-			strcpy_s(name, sourceName);
+			strcpy(name, sourceName); //***problem
+		}
+		else if (sourceName == nullptr) {
 
-			int count = numOfPhones;
+			*this = Contact();
+		}
 
-			for (int i = 0; i < size; i++) {
-				bool isValid = validCheck(sourcePhone[i]);
+		for (int i = 0; i < size; i++) {
 
-				if (isValid == true) {
-					phoneNum[count] = sourcePhone[i];
-					count++;
-					numOfPhones = count;
-				}
+			valid = validCheck(sourcePhone[i]);
+
+			if (valid == true) {
+
+				phoneNum[numOfPhones] = sourcePhone[i];
+				numOfPhones++;
 			}
 		}
 	}
@@ -40,24 +45,53 @@ namespace sict {
 		//default destructor will delete all dynamic allocated memory
 	}
 
-/*	Contact::Contact(const Contact&) {
+	Contact::Contact(const Contact& newContact) {
 
+		phoneNum = nullptr;
+		*this = newContact;
 	}
-	Contact& Contact::operator=(const Contact&) {
+
+	Contact& Contact::operator=(const Contact& newContact) {
+
+		//self-assignment to check for address of the newContact, make sure they are not same
+		if (this != &newContact) {
+			//shallow copies
+			strcpy(name, newContact.name);
+			numOfPhones = newContact.numOfPhones;
+
+			//allocate memory for phoneNum array
+			if (newContact.phoneNum != nullptr) {
+				phoneNum = new long long[numOfPhones];
+
+				for (int i = 0; i < numOfPhones; i++) {
+					phoneNum[i] = newContact.phoneNum[i];
+				}
+			}
+
+			else {
+				*this = Contact();
+			}
+		}
 		return *this;
 	}
+
 	Contact& Contact::operator+=(const long long newPhone) {
 
-		bool empty = isEmpty();
+	
 		bool valid = validCheck(newPhone);
+		int count = numOfPhones;
+		cout << "num " << numOfPhones;
+		if (valid == true) {
+			phoneNum = new long long[numOfPhones++];
 
-		if (empty == false) {
-
-			phoneNum[numOfPhones++] = newPhone;
+			phoneNum[count -1] = newPhone;
+			cout << "new" << newPhone << "phoneNum" << phoneNum << endl;
+			
+			numOfPhones++;
 		}
 
 		return *this;
-	}*/
+	}
 
 	bool Contact::validCheck(const long long toCheck) {
 
@@ -69,16 +103,15 @@ namespace sict {
 		int last4Digits = 0;
 
 		extractNum(toCheck, countryCode, areaCode, last3Digits, last4Digits);
-		isValid = areaCode >= 100 && countryCode >= 1 && last3Digits >= 100;
-		cout << "(+" << countryCode << ") " << areaCode << " " << last3Digits << "-" << last4Digits << endl;
-		cout << "is valid" << isValid << endl;
+		isValid = areaCode >= 100 && countryCode >= 1 && countryCode <= 99 && last3Digits >= 100;
+		//cout << "(+" << countryCode << ") " << areaCode << " " << last3Digits << "-" << last4Digits << endl;
+	//	cout << "is valid" << isValid << endl;
 		return isValid;
 	}
 
 
-	void Contact::extractNum (const long long phone, int& country, int& area, int& last3, int& last4) const {
+	void Contact::extractNum(const long long phone, int& country, int& area, int& last3, int& last4) {
 
-		//cout << "cout" << phone << endl;
 		int temp = 0;
 
 		last4 = phone % 10000;
@@ -87,17 +120,14 @@ namespace sict {
 		temp = (temp - last3) / 1000;
 		area = temp % 1000;
 		country = (temp - area) / 1000;
-
-		cout << "cout" << country << ") " << area << " " << last3 << "-" << last4 << endl;
-
 	}
+
 
 	bool Contact::isEmpty() const {
 
 		bool isEmpty = true;
 
-		if (name[0] != '\0' && numOfPhones != 0) {
-
+		if (name[0] != '\0') {
 			isEmpty = false;
 		}
 
@@ -105,29 +135,27 @@ namespace sict {
 	}
 
 
-	void Contact::display() const {
+	void Contact::display() {
 
 		bool empty = isEmpty();
 
-		if (empty == false) {
+		int countryCode = 0;
+		int areaCode = 0;
+		int last3Digits = 0;
+		int last4Digits = 0;
 
-			int countryCode = 0;
-			int areaCode = 0;
-			int last3Digits = 0;
-			int last4Digits = 0;
+		if (empty == false) {
 
 			cout << name << endl;
 
 			for (int i = 0; i < numOfPhones; i++) {
-
 				extractNum(phoneNum[i], countryCode, areaCode, last3Digits, last4Digits);
 				cout << "(+" << countryCode << ") " << areaCode << " " << last3Digits << "-" << last4Digits << endl;
 			}
 		}
 
-		else if (empty == true) {
-
-			cout << "Empty contact!" << endl;
+		else {
+			cout << "Empty Contact!" << endl;
 		}
 	}
 
