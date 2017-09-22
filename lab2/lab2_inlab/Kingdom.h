83bc704623d3794bdd100b83af4630b7bb47aa3d
nikki-thn <ntruong4@myseneca.ca>
2017-09-22 13:42:02 -0400

    Update Kingdom.h

--- lab2/lab2_inlab/Kingdom.h
+++ lab2/lab2_inlab/Kingdom.h
@@ -1,13 +1,21 @@
-#ifndef SICT_KINGDOM_H
+/*
+Nikki Truong - 112 214 174
+OOP244 - Fall 2017
+Workshop 2 - In lab
+*/
+
+
+#ifndef SICT_KINGDOM_H //header guard
 #define SICT_KINGDOM_H
 
 namespace sict {
 
+	//declare struct Kingdom
 	struct Kingdom {
 
 		char m_name[32];
 		int m_population;
-};
+	};
 
 	void display (const Kingdom);
 
@@ -16,5 +24,5 @@ namespace sict {
 }
 
 
-#endif // !SICT_KINGDOM_H
+#endif 
 
