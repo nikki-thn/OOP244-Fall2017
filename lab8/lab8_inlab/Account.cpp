9d4aabad8eaca54c7b7d99a82efb1efae7ea0d42
nikki-thn <ntruong4@myseneca.ca>
2017-09-23 12:36:51 -0400

    Update Account.cpp

--- lab8/lab8_inlab/Account.cpp
+++ lab8/lab8_inlab/Account.cpp
@@ -4,22 +4,46 @@ using namespace std;
 
 namespace ict{ 
 	
+	//default constructor
+	Account::Account() {
+		m_balance = 0.0;
+	}
+
 	// constructor   
-	
+	Account::Account (double balance) {
+		if (balance >= 0.0) {
+			m_balance = balance;
+		}
+		else {
+			m_balance = 1.0;
+		}
+	}
 
 	// credit (add) an amount to the account balance
-
+	void Account::credit (double credit) {
+		m_balance += credit;
+	}
 	
 	// debit (subtract) an amount from the account balance return bool 
+	bool Account::debit  (double debit) {
 
+		bool exceed = false;
+		if (debit <= m_balance) {
+			m_balance -= debit;
+		}
+		else if (debit > m_balance) {
+			exceed = true;
+		}
+		return exceed;
+	}
 
 	double Account::getBalance() const
 	{
-		return balance;
+		return m_balance;
 	} 
 
 	void Account::setBalance( double newBalance )
 	{
-		balance = newBalance;
+		m_balance = newBalance;
 	} 
 }
