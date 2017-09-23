279370845065e81ce6225aa8bf8c93988e0a30eb
nikki-thn <ntruong4@myseneca.ca>
2017-09-23 10:31:07 -0400

    Create SavingsAccount.h

---
+++ lab8/lab8_inlab/SavingsAccount.h
@@ -0,0 +1,18 @@
+#ifndef SICT_SAVINGSACCOUNT_H__
+#define SICT_SAVINGSACCOUNT_H__
+
+#include "Account.h"
+
+using namespace std;
+
+namespace ict{
+	class SavingsAccount : public Account{
+		private:
+    		double interestRate; // interest rate (percentage) 
+		public:
+		
+			// TODO: put prototypes here
+
+   };
+};
+#endif
