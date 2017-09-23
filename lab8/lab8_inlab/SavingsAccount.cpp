4dae547c9220b49cd4ac2ded192ac19d83675767
nikki-thn <ntruong4@myseneca.ca>
2017-09-23 12:37:28 -0400

    Update SavingsAccount.cpp

--- lab8/lab8_inlab/SavingsAccount.cpp
+++ lab8/lab8_inlab/SavingsAccount.cpp
@@ -1,8 +1,48 @@
+#include <iostream>
 #include "SavingsAccount.h"
+
 
 using namespace std;
 
 namespace ict{ 
 	
 	// TODO: Implement SavingsAccount member functions here
-}
+	//constructor
+	SavingsAccount::SavingsAccount (double balance, double interestRate) : Account (balance) {
+		
+		if (balance >= 0) {
+			setBalance(balance);
+		}
+		else {
+			setBalance(1.0);
+		}
+
+		if (interestRate < 0) {
+			m_interestRate = 0;
+		}
+		else if (interestRate > 0) {
+			m_interestRate = interestRate;
+		}
+
+	}
+
+	//function returns amount of interest earns by an account
+	double SavingsAccount::calculateInterest () {
+		
+		double interestAmt = 0.0;
+
+		interestAmt = getBalance() * m_interestRate;
+
+		return interestAmt;
+	}
+
+	//function display account info
+	void SavingsAccount::display (ostream& os) const {
+		//Account::display(os);
+		cout << "Account type: Saving" << endl;
+		cout.setf(ios::fixed);
+		cout.precision(2);
+		cout << "Balance: $ " << getBalance() << endl;
+		cout << "Interest rate (%): " << m_interestRate * 100 << endl;
+	}
+};
