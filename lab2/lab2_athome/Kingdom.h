c6d06f4dd166a5de2bdf7df6072d4eb67d8260d3
nikki-thn <ntruong4@myseneca.ca>
2017-09-22 20:41:07 -0400

    Update Kingdom.h

--- lab2/lab2_athome/Kingdom.h
+++ lab2/lab2_athome/Kingdom.h
@@ -1,21 +1,34 @@
-#ifndef SICT_KINGDOM_H
+/*
+Nikki Truong - 112 214 174
+OOP244 - Fall 2017
+Workshop 2 - In lab
+*/
+
+
+#ifndef SICT_KINGDOM_H //header guard
 #define SICT_KINGDOM_H
 
 namespace sict {
 
+	//declare struct Kingdom
 	struct Kingdom {
 
-		char m_name[32];
+		char m_name[31 + 1];
 		int m_population;
-};
+	};
 
-	void display (const Kingdom);
+	//the function receive a unmodifiable reference to object type Kingdom
+	//and print out kingdom's name and poppulation
+	void display(const Kingdom);
 
+
+        //the function receive a pointer (address of array of Kingdom) and size of array
+	//and print out all elements of the array as specified
 	void display(const Kingdom*, int numElements);
 
 
 }
 
 
-#endif // !SICT_KINGDOM_H
+#endif 
 
