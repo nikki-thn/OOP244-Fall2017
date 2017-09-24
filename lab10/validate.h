e24b822be2b3b49c0db003a6b28593db78175cbf
nikki-thn <ntruong4@myseneca.ca>
2017-09-24 11:49:57 -0400

    Update validate.h

--- lab10/validate.h
+++ lab10/validate.h
@@ -6,12 +6,16 @@ namespace ict {
 	bool validate(T min, T max, T testArr[], int numElement, bool arr[]){
 		bool isValid = false;
 
+		cout << "min " << min << "max " << max << endl;
 		for (int i = 0; i < numElement; i++) {
 			if (testArr[i] <= max && testArr[i] >= min) {
+				cout << "test "<< testArr[i] << endl;
 				isValid = true;
-				arr[i] = !isValid;
+				arr[i] = true;
 			}
-
+			else {
+				arr[i] = false;
+			}
 		}
 
 		return isValid;
