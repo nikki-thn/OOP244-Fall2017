41bfa2a85571f4dcc246438ff4a5672a71e036be
nikki-thn <ntruong4@myseneca.ca>
2017-09-24 10:57:56 -0400

    Create validate.h

---
+++ lab10/validate.h
@@ -0,0 +1,15 @@
+#ifndef ICT_VALIDATE_H
+#define ICT_VALIDATE_H
+
+namespace ict {
+	template <typename T>
+	bool validate(T a, T b, T[], T d, T e[]){
+		bool isValid;
+
+
+
+
+	}
+
+}
+#endif // !VALIDATE_H
