257ea2ea434a764095f1ec082699138a1bca220f
nikki-thn <ntruong4@myseneca.ca>
2017-09-25 12:13:45 -0400

    Create Firestruck.cpp

---
+++ Example/Firestruck.cpp
@@ -0,0 +1,24 @@
+
+//Main.cpp
+int main() {
+
+return 0;
+}
+
+
+//Firestruck.h
+
+#ifndef FIRESTRUCK_H
+#define FIRESTRUCT_H
+
+namespace safety {
+
+class Firestruck {
+
+char* m_pColor;
+
+
+};
+
+}
+#endif
