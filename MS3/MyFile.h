dc55670013e84438e8a7fe8d4e38076fa7e9e392
nikki-thn <ntruong4@myseneca.ca>
2017-09-27 15:57:14 -0400

    Create MyFile.h

---
+++ MS3/MyFile.h
@@ -0,0 +1,21 @@
+#ifndef SICT_MYFILE_H__
+#define SICT_MYFILE_H__
+#include "Streamable.h"
+#include "Streamable.h"
+#include "Streamable.h" // Streamable.h is included three times on purpose.
+namespace sict{
+  class MyFile : public Streamable {
+    char fname_[256];
+    char text_[10000];
+  public:
+    MyFile(const char* fname);
+    std::fstream& store(std::fstream& file, bool addNewLine)const;
+    std::fstream& load(std::fstream& file);
+    std::ostream& write(std::ostream& os, bool linear)const;
+    std::istream& read(std::istream& is);
+    void print();
+  };
+  std::ostream& operator<<(std::ostream& ostr, const MyFile& mf);
+  std::istream& operator>>(std::istream& istr, MyFile& mf);
+}
+#endif
