165e964ce40f63cd05210b285200624588576b89
nikki-thn <ntruong4@myseneca.ca>
2017-09-27 15:56:02 -0400

    Update ErrorMessage.cpp

--- MS2/ErrorMessage.cpp
+++ MS2/ErrorMessage.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include "ErrorMessage.h"
 #define _CRT_SECURE_NO_WARNINGS
 
@@ -8,31 +9,87 @@ namespace sict {
 
 	ErrorMessage::ErrorMessage() {
 
-	}
-	ErrorMessage::ErrorMessage(const char*) {
+		message_ = nullptr;
 
 	}
-	ErrorMessage::ErrorMessage(const ErrorMessage&) {
+
+	ErrorMessage::ErrorMessage(const char* errorMessage) {
+
+		message_ = nullptr;
+		message(errorMessage);
+
 	}
-	virtual ErrorMessage::~ErrorMessage(){
+	//	ErrorMessage::ErrorMessage(const ErrorMessage&) {
+
+	ErrorMessage::~ErrorMessage() {
+
+		delete[] message_;
+
 	}
 
-	ErrorMessage& ErrorMessage::operator= (const ErrorMessage&) = delete{
-	}
-	ErrorMessage& ErrorMessage::operator= (const char*){
+	//ErrorMessage& ErrorMessage::operator= (const ErrorMessage&) = delete{
+
+	ErrorMessage& ErrorMessage::operator= (const char* errorMessage) {
+
+		clear();
+		message(errorMessage);
+
+		return *this;
 	}
 
-	void ErrorMessage::clear(){
-	}
-	bool ErrorMessage::isClear() const{
-	}
-	void ErrorMessage::message(const char*){
-	}
-	const char* ErrorMessage::message()const{
+	void ErrorMessage::clear() {
+
+		delete[] message_;
+		message_ = nullptr;
+	
 	}
 
-	std::ostream& ErrorMessage::write(std::ostream& ostr) const{
-	         }
+	bool ErrorMessage::isClear() const {
+		bool isEmpty = false;
+
+		if (message_ == nullptr) {
+			isEmpty = true;
+		}
+
+		return isEmpty;
+	}
+
+	void ErrorMessage::message(const char* value) {
+
+		clear();
+		message_ = new char[strlen(value) + 1];
+		strcpy(message_, value);
+
+	}
+
+	const char* ErrorMessage::message()const {
+
+		return message_;
+		//cout << "const char* ErrorMessage::message()const" << endl;
+	}
+
+	//take from lab10
+	std::ostream& ErrorMessage::display(std::ostream& os)const {
+	
+
+		bool isEmpty = isClear();
+
+		if (isEmpty == false) {
+			cout << message_ << endl;
+		}
+
+		else {
+			cout << "object is empty" << endl;
+		}
+
+		return os;
+	}
+	
+
+	std::ostream& operator<< (std::ostream& os, ErrorMessage& temp) {
+
+		return temp.display(os);
+	}
 
 
 
