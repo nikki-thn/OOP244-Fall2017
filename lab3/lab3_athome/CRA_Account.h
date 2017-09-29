7e5a174cc861f28515c62665493f7de6eb5e89d8
nikki-thn <ntruong4@myseneca.ca>
2017-09-29 16:09:54 -0400

    Update CRA_Account.h

--- lab3/lab3_athome/CRA_Account.h
+++ lab3/lab3_athome/CRA_Account.h
@@ -9,31 +9,32 @@ Workshop 3 - at home
 
 
 namespace sict {
-	
-const int max_name_length = 40;
-const int min_sin = 100000000;
-const int max_sin = 999999999;
-const int max_yrs = 4;
-	
+
+	const int max_name_length = 40;
+	const int min_sin = 100000000;
+	const int max_sin = 999999999;
+	const int max_yrs = 4;
+
 	class CRA_Account {
 
-		char m_lastName [max_name_length + 1];
-		char m_firstName [max_name_length + 1];
+		char m_lastName[max_name_length + 1];
+		char m_firstName[max_name_length + 1];
 		int m_sin;
-		int m_year [max_yrs];
-		double m_balance [max_yrs];
+		int m_year[max_yrs];
+		double m_balance[max_yrs];
 		int m_years;
 
 
 	public:
-                void set(const char* familyName, const char* givenName, int sin); //to set values for an object or set empty state
-		bool isEmpty() const; //check if the object is empty
-                void display() const; //print out data members
 
+		void set(const char* familyName, const char* givenName, int sin); //to set values for an object or set empty state
 		void set(int year, double balance); //to set values of newly added data members
+		bool isEmpty() const; //check if the object is empty
+		void display() const; //print out data members
+
 
 	};
-        
+
 	//helper function to validate SIN
 	void bubbleSort(int a[], int n);
 
