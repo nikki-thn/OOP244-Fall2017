4ab1e699531af1064afb357ba890bf5ef37b1468
nikki-thn <ntruong4@myseneca.ca>
2017-09-29 12:03:05 -0400

    Update CRA_Account.h

--- lab3/lab3_inlab/CRA_Account.h
+++ lab3/lab3_inlab/CRA_Account.h
@@ -7,11 +7,14 @@ Workshop 3 - in lab
 #ifndef SICT_CRA_ACCOUNT_H
 #define SICT_CRA_ACCOUNT_H
 
+
+namespace sict {
+	
+	
 #define MAX_NAME_LENGTH 40
 #define MIN_SIN 100000000
 #define MAX_SIN 999999999
 
-namespace sict {
 
 	class CRA_Account {
 
