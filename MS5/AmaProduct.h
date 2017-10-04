afddbda8bf3f7e17cd23253cdda7f481153f46cb
nikki-thn <ntruong4@myseneca.ca>
2017-10-03 23:49:27 -0400

    Update AmaProduct.h

--- MS5/AmaProduct.h
+++ MS5/AmaProduct.h
@@ -1,3 +1,41 @@
-// AmaProduct.h
+#ifndef SICT_AMAPRODUCT_H
+#define SICT_AMAPRODUCT_H
+
+#include <iostream>
+#include "Product.h"
+#include "ErrorMessage.h"
+
+using namespace std;
+
+namespace sict {
+
+	class AmaProduct : public Product {
+		
+		char fileTag_;
+		char unit_[11];
+
+	protected:
+		int err_;
+		AmaProduct(const char fileTag);
+
+	public:
+
+		AmaProduct();
+	
+		bool isClear() const;
+		const char* unit() const { return unit_; } //return type issue
+		void unit(const char* value);
+		fstream& store(fstream& file, bool addNewLine = true)const;
+		fstream& load(fstream& file);
+		ostream& write(ostream& os, bool linear)const;
+		istream& read(istream& istr);
+	};
+
+	istream& operator>> (std::istream& is, AmaProduct& s);
+	ostream& operator<< (std::ostream& is, const AmaProduct& s);
+}
+
+
+#endif
 
 
