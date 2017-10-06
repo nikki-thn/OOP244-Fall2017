1ad96e4f22183c1d89e4b3673e87594ed783f232
nikki-thn <ntruong4@myseneca.ca>
2017-10-06 13:28:27 -0400

    Update Passenger.h

--- lab4/lab4_inlab/Passenger.h
+++ lab4/lab4_inlab/Passenger.h
@@ -4,27 +4,23 @@
 
 #define SIZE 32
 
-
 // TODO: declare your namespace here
 namespace sict {
 
 	// TODO: define the Passenger class here
 	class Passenger {
 
-		char name[SIZE + 1];
-		char destination[SIZE + 1];
+		char m_name[SIZE];
+		char m_destination[SIZE];
 
 	public:
 
 		Passenger(); //default constructor
 		Passenger(const char *name_, const char* destination_); //overload constructor
-
-		bool isEmpty() const;
-		void display() const;
-
-
+		
+		bool isEmpty() const; //to check if object is empty
+		void display() const; //display
 	};
-
 }
 
 #endif
