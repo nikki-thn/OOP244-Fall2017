82dd24a9f5bafa17b5b3b9a2df4848cb31241970
nikki-thn <ntruong4@myseneca.ca>
2017-10-06 14:21:57 -0400

    Update Passenger.cpp

--- lab4/lab4_inlab/Passenger.cpp
+++ lab4/lab4_inlab/Passenger.cpp
@@ -13,53 +13,53 @@ Workshop 4 - In_lab
 // TODO: continue your namespace here
 using namespace std;
 
-namespace sict{
+namespace sict {
 
 
-// TODO: implement the default constructor here
-Passenger::Passenger() {
+	// TODO: implement the default constructor here
+	Passenger::Passenger() {
 
-	m_name[0] = '\0';
-	m_destination[0] = '\0';
-}
-
-// TODO: implement the constructor with 2 parameters here
-Passenger::Passenger(const char* name, const char* destination) {
-
-	if (m_name != nullptr && destination != nullptr) {
-		strcpy(m_name, name);
-		strcpy(m_destination, destination);
-	}
-	else {
 		m_name[0] = '\0';
-	        m_destination[0] = '\0';
-	}
-}
-
-
-// TODO: implement isEmpty query here
-bool Passenger::isEmpty() const {
-
-	bool isEmpty = true;
-	if (m_name[0] != '\0' && m_destination[0] != '\0') {
-		isEmpty = false;
+		m_destination[0] = '\0';
 	}
 
-	return isEmpty;
-}
+	// TODO: implement the constructor with 2 parameters here
+	Passenger::Passenger(const char* name, const char* destination) {
+
+		if (name != nullptr && destination != nullptr) {
+			strcpy(m_name, name);
+			strcpy(m_destination, destination);
+		}
+		else {
+			m_name[0] = '\0';
+			m_destination[0] = '\0';
+		}
+	}
 
 
-// TODO: implement display query here
-void Passenger::display() const {
-	
-	bool notEmpty = isEmpty();
-	if (notEmpty == true){
-		cout << m_name << " - " << m_destination << endl;
+	// TODO: implement isEmpty query here
+	bool Passenger::isEmpty() const {
+
+		bool isEmpty = true;
+		if (m_name[0] != '\0' && m_destination[0] != '\0') {
+			isEmpty = false;
+		}
+
+		return isEmpty;
 	}
-	else {
-		cout << "No Passengers!" << endl;
+
+
+	// TODO: implement display query here
+	void Passenger::display() const {
+
+		bool empty = isEmpty();
+		if (empty == false) {
+			cout << m_name << " - " << m_destination << endl;
+		}
+		else {
+			cout << "No Passengers!" << endl;
+		}
 	}
-}
 
 
 }
