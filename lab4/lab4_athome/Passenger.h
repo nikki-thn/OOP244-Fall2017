60eb4ef3f43e9d13e64826ccff5927cca7dfdca3
nikki-thn <ntruong4@myseneca.ca>
2017-10-06 14:29:54 -0400

    Update Passenger.h

--- lab4/lab4_athome/Passenger.h
+++ lab4/lab4_athome/Passenger.h
@@ -1,9 +1,14 @@
+/*
+Nikki Truong - 112 214 174
+OOP244 - Fall 2017
+Workshop 4 - In_lab
+*/
+
 // TODO: add header file guard here 
 #ifndef SICT_PASSENGER_H
 #define SICT_PASSENGER_H
 
 #define SIZE 32
-
 
 // TODO: declare your namespace here
 namespace sict {
@@ -11,8 +16,9 @@ namespace sict {
 	// TODO: define the Passenger class here
 	class Passenger {
 
-		char passengerName[SIZE + 1];
-		char destination[SIZE + 1];
+		char m_name[SIZE];
+		char m_destination[SIZE];
+
 		int departYear;
 		int departMonth;
 		int departDay;
@@ -20,16 +26,14 @@ namespace sict {
 	public:
 
 		Passenger(); //default constructor
-		Passenger(const char* name_, const char* destination_); //constructor of 2 parameters
+		Passenger(const char *, const char*); //overload constructor with 2 parameters
 		Passenger(const char *name_, const char* destination_, int day, int month, int year); //constructor with 5 parameters
+		
+		bool isEmpty() const; //to check if object is empty
+		void display() const; //display
 
-		bool isEmpty() const;
-		void display() const;
-
-		const char* name() const;
-		const Passenger& Passenger::address() const;
+		const char* name() const; //return address of name or empty address if object is empty
 		bool canTravelWith(const Passenger&) const;
-
 
 	};
 
