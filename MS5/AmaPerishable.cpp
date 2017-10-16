b8c026ccc6071af42464eae1fd690c1722a9351a
nikki-thn <ntruong4@myseneca.ca>
2017-10-15 22:46:13 -0400

    Update AmaPerishable.cpp

--- MS5/AmaPerishable.cpp
+++ MS5/AmaPerishable.cpp
@@ -26,44 +26,11 @@ namespace sict {
 	
 	std::fstream& AmaPerishable::load(std::fstream& file) {
 		
-		//AmaProduct::load(file);
-		
-		//Date::read(file);
+		AmaProduct::load(file);
 	
-		//char input[100]; 
+                file >> expiry_;
 
-		//file.getline(input, 100, '\n');
-               // file >> expiry_;
-
-		//file.ignore();
-		
-		//cout << input << endl;
-		
-		
-		double priceIn;
-		bool taxedIn;
-		int qtyIn, qtyNeededIn;
-		char skuIn[MAX_SKU_LEN + 1], nameIn[21];
-		char a;
-
-		file.getline(skuIn, MAX_SKU_LEN, ',');
 		file.ignore();
-		sku(skuIn);
-		file.getline(nameIn, 20, ',');
-		name(nameIn);
-		file.ignore();
-		file >> priceIn >> a;
-		price(priceIn);
-		file >> taxedIn >> a;
-		taxed(taxedIn);
-		file >> qtyIn >> a;
-		quantity(qtyIn);
-		file.getline(unit_, 10, ',');
-		file.ignore();
-		file >> qtyNeededIn >> a;
-		qtyNeeded(qtyNeededIn);
-
-		return file;
 
 		return file;
 	}
