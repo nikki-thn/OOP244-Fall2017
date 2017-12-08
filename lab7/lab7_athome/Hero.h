f54a7ae8d862f34ef1efd8953263d2d85ea97933
nikki-thn <ntruong4@myseneca.ca>
2017-12-08 15:18:16 -0500

    Update Hero.h

--- lab7/lab7_athome/Hero.h
+++ lab7/lab7_athome/Hero.h
@@ -1,35 +1,24 @@
-#ifndef HERO_H
-#define HERO_H
-
-#include <iostream>
+#ifndef SICT_SUPERHERO_H
+#include "Hero.h"
 
 namespace sict {
-	class Hero {
-		char m_name[41];
-		int  m_attack;
-		int  m_maximumHealth;
-		int  m_health;
 
-		bool isEmpty() const;
+	class SuperHero : public Hero {
+
+		int m_bonusAttack;
+		int m_defend;
 
 	public:
-		// constructors
-		Hero();
-		Hero(const char name[], int maximumHealth, int attack);
 
-		// member functions
-		bool isAlive() const { return m_health > 0; }
-		void respawn();
-		void deductHealth(int);
-		void display(std::ostream&) const;
-		int  getAttack() const { return m_attack; }
+		SuperHero();
+		SuperHero(const char name[], int health, int attack, int bonusAttack, int defend);
+		
+		int attackStrength() const { return this->attackStrength + m_bonusAttack; }
+		int defend() const { return m_defend; }
 
-		// friend helper function to insert name into ostream
-		friend std::ostream& operator<<(std::ostream&, const Hero&);
 	};
 
-    void applyDamage (Hero& A, Hero& B);
-    const Hero & operator*(const Hero &, const Hero &);
+	const SuperHero& operator* (const SuperHero& first, const SuperHero& second);
 }
 
 #endif
