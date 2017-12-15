7f1042a51e086a1ec17b4c449091a576a1ef0a5f
nikki-thn <ntruong4@myseneca.ca>
2017-12-15 14:48:36 -0500

    Update ChequingAccount.h

--- lab8_Fall2017/in_lab/ChequingAccount.h
+++ lab8_Fall2017/in_lab/ChequingAccount.h
@@ -1,7 +1,7 @@
 #ifndef SICT_CHEQUING_ACCOUNT_H
 #define SICT_CHEQUING_ACCOUNT_H 
 #include <iostream>
-#include "C:\Users\Nikki\Downloads\OOP244-Fall2017\lab8_fall2017\Account.h"
+#include "Account.h"
 
 namespace sict {
 
@@ -12,8 +12,6 @@ namespace sict {
 
 	public:
 		ChequingAccount(double balance = 0.0, double transactionFee = 0.0, double monthlyFee = 0.0);
-
-
 		bool credit(double);
 		bool debit(double);
 		void monthEnd();
