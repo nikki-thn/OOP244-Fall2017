d4c6f04051901a1e0356745d2b6eefaa2191ea90
nikki-thn <ntruong4@myseneca.ca>
2017-12-21 23:09:07 -0500

    Add files via upload

---
+++ lab9_f17/Data.cpp
@@ -0,0 +1,118 @@
+// OOP244 Workshop 9: Function Templates
+// File: Data.cpp
+// Version: 2.0.1
+// Date: 2017/12/17
+// Author: Chris Szalwinski, based on previous work by Hasan Murtaza
+// Description:
+// This file defines the answers and readRow functions
+// Chris     2017.12.17 removed answers to Q1
+///////////////////////////////////////////////////
+
+#include "Data.h"
+
+namespace sict {
+	// readRow extracts a single record from input and stores n data fields in data
+	// - includes error checking: checks if the name of the row is valid.
+	//
+	bool readRow(std::istream& input, const char* name, double* data, int n) {
+		char rowname[2000];
+		bool ok = !input.bad();
+
+		// row format is 
+		// name,1,2,3,4,5,6
+
+		if (ok) {
+			// read the name  first
+
+			input.get(rowname, 2000, ',');
+			// note: get will read a string upto the , character
+			// it will leave the , character in the input buffer 
+
+			// check that the caller is reading the correct row (name)
+			// - if the user is reading the wrong row, return an error
+			if (std::strcmp(rowname, name) != 0) {
+				// wrong row
+				std::cout << "Cannot parse row for " << name << std::endl;
+				input.ignore(2000, '\n');
+				ok = false;
+			}
+		}
+
+		if (ok)
+			// read the data array
+			ok = read(input, data, n);
+
+		// finish the line
+		input.ignore(2000, '\n');
+		return ok;
+	}
+
+	bool readRow(std::istream& input, const char* name, int* data, int n) {
+		char rowname[2000];
+		bool ok = !input.bad();
+
+		// row format is 
+		// name,1,2,3,4,5,6
+
+		if (ok) {
+			// read the name  first
+
+			input.get(rowname, 2000, ',');
+			// note: get will read a string upto the , character
+			// it will leave the , character in the input buffer 
+
+			// check that the caller is reading the correct row (name)
+			// - if the user is reading the wrong row, return an error
+			if (std::strcmp(rowname, name) != 0) {
+				// wrong row
+				std::cout << "Cannot parse row for " << name << std::endl;
+				input.ignore(2000, '\n');
+				ok = false;
+			}
+		}
+
+		if (ok)
+			// read the data array
+			ok = read(input, data, n);
+
+		// finish the line
+		input.ignore(2000, '\n');
+		return ok;
+	}
+
+	// answers outputs statistics for visual validation of data
+	//
+	void answers(const int* year, const int* population, const int* violentCrime,
+		const double* violentCrimeRate, const int* grandTheftAuto, const int n) {
+
+		// Q1: print the population change in millions of people
+		// - print answer correct to 2 decimal places.
+		// e..g "6.31 million";
+		// note that population is an integer, so dividing by 1000000 would yield "6"
+
+		std::cout.precision(2);
+		std::cout << population;
+
+
+
+
+		// Q2. print whether violent crime rate has gone up or down between 2000 and 2005
+		std::cout << "Violent Crime trend is down" << std::endl;
+
+
+
+
+
+		// Q3 print the GTA number accurate to 0 decimal places
+		std::cout << "There are" << grandTheftAuto << "million Grand Theft Auto ncidents on average a year";
+		std::cout << std::endl;
+
+
+
+		// Q4. Print the min and max violentCrime rates
+		std::cout << "The Minimum Violent Crime rate was" << min(violentCrimeRate, n);
+		std::cout << "The Maximum Violent Crime rate was" << max(violentCrimeRate, n);
+
+
+	}
+}
---
+++ lab9_f17/Data.h
@@ -0,0 +1,110 @@
+// OOP244 Workshop 9: Function Templates
+// File: Data.h
+// Version: 2.0
+// Date: 2017/12/15
+// Author: Chris Szalwinski, based on previous work by Hasan Murtaza
+// Description:
+// This file defines the templated functions and the answers prototype
+///////////////////////////////////////////////////
+
+#ifndef SICT_DATA_H
+#define SICT_DATA_H
+
+#include <iostream>
+
+namespace sict {
+
+	const int LARGEST_NUMBER = 1000000000; // for min
+	const int SMALLEST_NUMBER = 0; // for max
+	const int COLUMN_WIDTH = 15; //output width
+
+	// max returns the largest item in data
+	//
+	template<typename T>
+	T max(const T* data, int n) {
+		T largest;
+		for (int i = 0; i < n; i++) {
+			if (data[i] > data[i + 1]) largest = data[i];
+		}
+		return largest;
+	}
+
+
+	// min returns the smallest item in data
+	//
+	template<typename T>
+	T min(const T* data, int n) {
+		T smallest;
+		for (int i = 0; i < n; i++) {
+			if (data[i] < data[i + 1]) smallest = data[i];
+		}
+		return smallest;
+	}
+
+
+	// sum returns the sum of n items in data
+	//    
+	template<typename T>
+	T sum(const T* data, int n) {
+		T total;
+		for (int i = 0; i < n; i++) {
+			total = total + data[i];
+		}
+		return total;
+	}
+
+	// average returns the average of n items in data
+	//
+	template<typename T>
+	double average(const T* data, int n) {
+		double avg;
+		for (int i = 0; i < n; i++) {
+			avg = avg + data[i]; //get total first
+		}
+		avg /= n; //divide toal / n to get average
+		return total;
+	}
+
+	// read extracts n items from input and stores them in data
+	// - the items are comma separated
+	// - this function assumes that a comma exists at the next field to be read
+	// - for example: ,data1, data2,data3, ...., datan
+	//
+	template<typename T>
+	bool read(std::istream& input, T* data, int n) {
+		
+		bool success = true;
+		
+		for (int i = 0; i < n; i++) {
+			input << data[i];
+			input.ignore();
+
+			if (input.fail()) success = false;	
+		}
+		return success;
+	}
+
+	// display inserts n items of data into std::cout
+	//
+	template<typename T>
+	void display(const char* name, const T* data, int n) {
+		std::cout.width(COLUMN_WIDTH);
+		std::cout.setf(ios::right);
+		std::cout << name;
+		std::cout.unsetf(ios::right);
+		
+		for (int i = 0; i < n; i++) {
+			std::cout.width(COLUMN_WIDTH);
+			std::cout.setf(ios::right);
+			std::cout << data[i];
+			std::cout.unsetf(ios::right);
+		}
+	}
+
+	bool readRow(std::istream& input, const char* name, int* data, int n);
+	bool readRow(std::istream& input, const char* name, double* data, int n);
+	void answers(const int*, const int*, const int*, const double*, const int*, const int n);
+
+}
+
+#endif
