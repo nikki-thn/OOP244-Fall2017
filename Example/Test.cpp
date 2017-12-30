ae346efeb20065a999b76d39bf27a50474e39ba5
nikki-thn <ntruong4@myseneca.ca>
2017-12-30 14:43:12 -0500

    Create Test.cpp

---
+++ Example/Test.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+
+
+using namespace std;
+
+class Test {
+
+	char member;
+
+public:
+
+	Test() {
+		cout << "constructor" << endl;
+	}
+
+	Test(char num) : member(num) {
+		cout << member << "1 para constructor" << endl;
+	}
+
+	~Test() {
+		cout << member << "destructor" << endl;
+	}
+
+	Test(const Test&) {
+		cout << member << "copy constructor" << endl;
+	}
+
+	Test& operator= (const Test& rhs) {
+		cout << member << "copy assignment" << endl;
+		return *this;
+	}
+
+	Test print(const Test rhs) {
+		cout << member << "print" << endl;
+		return *this;
+	}
+};
+
+int main() {
+
+	//Test a;
+	Test c('c');
+	Test b ('b');
+	//b = c;
+	b.print(c);
+
+	cout << "fkfd" << endl;
+
+	return 0;
+}
